15c6d2cfff01bb4f1c6cf5fa6c6a34c64cf5dced
Stefan Ivanovski <stefs.ivanovski@tuta.io>
2020-07-31 19:12:55 +0200

    Linux build

---
+++ build.sh
@@ -0,0 +1,1 @@
+cd sane_engine && g++ -O2 -Wall ../thirdparty/glad/src/glad.c src/main.cpp src/**/*.cpp -I src/ -I ../thirdparty/glad/include -I ../thirdparty -lglfw -ldl
---
+++ linux-prepare-build.sh
@@ -0,0 +1,1 @@
+sudo apt update && sudo apt upgrade && sudo apt install libglfw3-dev libglm-dev
